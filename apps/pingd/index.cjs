74c961dd246a4ba4c006b0f9c61205e4dd7d9960
Luca Cavallaro <luca.cavallaro@pagopa.it>
2026-09-09 16:08:26 +0200

    Add docker support

--- apps/pingd/index.cjs
+++ apps/pingd/index.cjs
@@ -1,6 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(morgan("tiny"));
 
 app.get("/ping", (req, res) => {
   res.status(200).json({
@@ -8,6 +11,15 @@ app.get("/ping", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log("listening to 3000...");
 });
+
+const close = () => {
+  server.close();
+  server.closeAllConnections();
+  console.log("bye!");
+}
+
+process.on("SIGINT", close);
+process.on("SIGTERM", close);
